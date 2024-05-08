688adbc2cf90e77d8f0270baaec2c6bcce206566
DarshilModi29 <132668473+DarshilModi29@users.noreply.github.com>
2024-05-08 16:53:45 +0530

    small commit

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT;
 const fetchProducts = require("./src/routers/fetchProducts");
+const paymentRouter = require("./src/routers/payment");
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 app.use(fetchProducts);
-
+app.use(paymentRouter);
 mongoose.connect(process.env.HOST)
     .then(() => {
         console.log("Connection Successful");
